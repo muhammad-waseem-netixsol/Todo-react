6425a227e514fb544093a98ef8016a6f9c036287
muhammad-waseem-netixsol <151015897+muhammad-waseem-netixsol@users.noreply.github.com>
2023-12-13 15:25:42 +0500

    Update Todo.js

--- src/components/list/Todo.js
+++ src/components/list/Todo.js
@@ -14,7 +14,7 @@ const Todo = (props) => {
         event.stopPropagation();
         props.onEdit(props.index);   
      };
-    return <div onClick={onClickHandler} className={`relative bg-todo ${props.todo.done && "line-through text-red-300"} px-3 py-5 shadow-xl cursor-pointer my-3`}>
+    return <div onClick={onClickHandler} className={`relative bg-todo ${props.todo.done && "line-through text-red-300"} px-3 py-5 shadow-xl cursor-pointer my-3 break-words`}>
         <h1 className={`${props.todo.done && "line-through text-red-300"} font-bold`}>{props.todo.heading}</h1>
         <p>{props.todo.details}</p>
         <span className="text-white block text-right"><i className="fa-regular fa-pen-to-square hover:text-red-500" onClick={onEditTodoHandler}></i></span>
